b26c64b830d7bfcf71ea5b2ac49cf7f26e5b17f2
alekhyacholleti <48060303+AlekhyaCholleti@users.noreply.github.com>
2020-01-09 11:39:43 +0530

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,10 +1,10 @@
 mkdir data
-wget http://msvocds.blob.core.windows.net/annotations-1-0-3/captions_train-val2014.zip -P ./data/
+wget http://images.cocodataset.org/annotations/annotations_trainval2014.zip -P ./data/
 wget http://images.cocodataset.org/zips/train2014.zip -P ./data/
 wget http://images.cocodataset.org/zips/val2014.zip -P ./data/
 
-unzip ./data/captions_train-val2014.zip -d ./data/
-rm ./data/captions_train-val2014.zip
+unzip ./data/annotations_trainval2014.zip -d ./data/
+rm ./data/annotations_trainval2014.zip
 unzip ./data/train2014.zip -d ./data/
 rm ./data/train2014.zip 
 unzip ./data/val2014.zip -d ./data/ 
